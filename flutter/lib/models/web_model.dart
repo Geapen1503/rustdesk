a3a7be458819c3028e3dfe39ebad4e311b9acd76
Geapen <theogilat@gmail.com>
2025-01-22 09:25:26 +0100

    Refactoring to the original name "RustDesk"

--- flutter/lib/models/web_model.dart
+++ flutter/lib/models/web_model.dart
@@ -20,7 +20,7 @@ typedef HandleEvent = Future<void> Function(Map<String, dynamic> evt);
 
 class PlatformFFI {
   final _eventHandlers = <String, Map<String, HandleEvent>>{};
-  final ItsystemsdeskImpl _ffiBind = ItsystemsdeskImpl();
+  final RustdeskImpl _ffiBind = RustdeskImpl();
 
   static String getByName(String name, [String arg = '']) {
     return context.callMethod('getByName', [name, arg]);
@@ -42,7 +42,7 @@ class PlatformFFI {
   static final PlatformFFI instance = PlatformFFI._();
 
   static get localeName => window.navigator.language;
-  ItsystemsdeskImpl get ffiBind => _ffiBind;
+  RustdeskImpl get ffiBind => _ffiBind;
 
   static Future<String> getVersion() async {
     throw UnimplementedError();
