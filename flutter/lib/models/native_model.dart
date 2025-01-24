8b35222723068dc32b62c7973f9087ce2e1d7c90
Geapen <theogilat@gmail.com>
2025-01-24 10:02:47 +0100

    Removing itsystemsdesk naming mods

--- flutter/lib/models/native_model.dart
+++ flutter/lib/models/native_model.dart
@@ -32,7 +32,7 @@ class PlatformFFI {
   // _homeDir is only needed for Android and IOS.
   String _homeDir = '';
   final _eventHandlers = <String, Map<String, HandleEvent>>{};
-  late ItsystemsdeskImpl _ffiBind;
+  late RustDeskImpl _ffiBind;
   late String _appType;
   StreamEventHandler? _eventCallback;
 
@@ -41,7 +41,7 @@ class PlatformFFI {
   static final PlatformFFI instance = PlatformFFI._();
   final _toAndroidChannel = const MethodChannel('mChannel');
 
-  ItsystemsdeskImpl get ffiBind => _ffiBind;
+  RustDeskImpl get ffiBind => _ffiBind;
   F3? _session_get_rgba;
 
   static get localeName => Platform.localeName;
@@ -139,7 +139,7 @@ class PlatformFFI {
       } catch (e) {
         debugPrint('Failed to get documents directory: $e');
       }
-      _ffiBind = ItsystemsdeskImpl(dylib);
+      _ffiBind = RustDeskImpl(dylib);
 
       if (isLinux) {
         if (isMain) {
@@ -236,10 +236,10 @@ class PlatformFFI {
   }
 
   /// Start listening to the Rust core's events and frames.
-  void _startListenEvent(ItsystemsdeskImpl itsystemsdeskImpl) {
+  void _startListenEvent(RustDeskImpl rustdeskImpl) {
     final appType =
         _appType == kAppTypeDesktopRemote ? '$_appType,$kWindowId' : _appType;
-    var sink = itsystemsdeskImpl.startGlobalEventStream(appType: appType);
+    var sink = rustdeskImpl.startGlobalEventStream(appType: appType);
     sink.listen((message) {
       () async {
         try {
